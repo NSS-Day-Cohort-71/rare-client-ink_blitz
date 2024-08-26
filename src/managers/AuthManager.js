8bbf0c4a9a22b5351461d17101c54a84691e89bc
Zach Mosher <zmosher4@gmail.com>
2024-08-26 15:58:02 -0500

    testing authentication

--- src/managers/AuthManager.js
+++ src/managers/AuthManager.js
@@ -1,24 +1,26 @@
 export const loginUser = (user) => {
-  return fetch("http://localhost:8088/login", {
-    method: "POST",
+  return fetch('http://localhost:8088/login', {
+    method: 'POST',
     headers: {
-      "Content-Type": "application/json",
-      "Accept": "application/json"
+      'Content-Type': 'application/json',
+      Accept: 'application/json',
     },
     body: JSON.stringify({
       username: user.username,
-      password: user.password
-    })
-  }).then(res => res.json())
-}
+      password: user.password,
+    }),
+  }).then((res) => res.json());
+};
 
 export const registerUser = (newUser) => {
-  return fetch("http://localhost:8088/register", {
-    method: "POST",
+  return fetch('http://localhost:8088/register', {
+    method: 'POST',
     headers: {
-      "Content-Type": "application/json",
-      "Accept": "application/json"
+      'Content-Type': 'application/json',
+      Accept: 'application/json',
     },
-    body: JSON.stringify(newUser)
-  }).then(res => res.json())
-}
+    body: JSON.stringify(newUser),
+  }).then((res) => {
+    return res.json();
+  });
+};
