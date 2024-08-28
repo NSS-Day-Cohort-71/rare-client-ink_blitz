7484dadcb057d86e50ff8ef07d01a15c629ad976
Jana Ismail <janaismail@janas-air.lan>
2024-08-28 13:51:31 -0500

    WIP

---
+++ src/components/categories/CreateCategory.jsx
@@ -0,0 +1,2 @@
+// define CreateCategory() function
+    
--- src/components/utils/HumanDate.js
+++ src/components/utils/HumanDate.js
@@ -1,4 +1,7 @@
 export const HumanDate = ({ date }) => {
+  if (!date) {
+    return 'Invalid Date';
+  }
   return new Date(date.replace(/-/g, '\/')).toLocaleDateString("en-US",
     {
       weekday: 'long',
