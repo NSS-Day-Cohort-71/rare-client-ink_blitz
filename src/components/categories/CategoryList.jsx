fa5b258151d72fa39f339c49d32eb7c3bedd693f
Zach Mosher <zmosher4@gmail.com>
2024-09-03 15:34:14 -0500

    added delete functionality to categories

--- src/components/categories/CategoryList.jsx
+++ src/components/categories/CategoryList.jsx
@@ -1,50 +1,58 @@
-
-import { useEffect, useState } from "react"
-import { getAllCategories } from "../../managers/CategoryManager"
-import { Link, useParams } from "react-router-dom"
+import { useEffect, useState } from 'react';
+import {
+  deleteCategory,
+  getAllCategories,
+} from '../../managers/CategoryManager';
+import { Link, useParams } from 'react-router-dom';
 
 // define CategoryList() function
-    // define 'categories, setCategories' variable with useState() hook
-    // define 'newCategory, setNewCategory' variable with useState() hook
+// define 'categories, setCategories' variable with useState() hook
+// define 'newCategory, setNewCategory' variable with useState() hook
 
-    // define getAndSetCategories() function that fetches categories with getAllCategories()
+// define getAndSetCategories() function that fetches categories with getAllCategories()
 
-    // define useEffect() hook that calls getAndSetCategories() on initial render
+// define useEffect() hook that calls getAndSetCategories() on initial render
 
-    // define useEffect() hook with newCategory in the dependency array
-    // that calls getAndSetCategories() again when a new category is created
+// define useEffect() hook with newCategory in the dependency array
+// that calls getAndSetCategories() again when a new category is created
 
-    // return JSX with list of categories as clickable items
+// return JSX with list of categories as clickable items
 
 export const CategoryList = () => {
-    const [categories, setCategories] = useState([])
-    // const [newCategory, setNewCategory] = useState()
+  const [categories, setCategories] = useState([]);
+  // const [newCategory, setNewCategory] = useState()
 
-    const getAndSetCategories = async () => {
-        const categoriesArr = await getAllCategories()
-        setCategories(categoriesArr)
-    }
+  const getAndSetCategories = async () => {
+    const categoriesArr = await getAllCategories();
+    setCategories(categoriesArr);
+  };
 
-    useEffect(() => {
-        getAndSetCategories()
-    }, [])
+  useEffect(() => {
+    getAndSetCategories();
+  }, []);
 
-    return (
-        <section>
-            <h1>Categories</h1>
-            {categories?.map((category) => {
-                return (
-                    <div key={category.id}>
-                        <ul>
-                            <Link to={`/categories/${category.id}`}>
-                                <li>
-                                    {category.label}
-                                </li>
-                            </Link>
-                        </ul>
-                    </div>
-                )
-            })}
-        </section>
-    )
-}
+  const handleDelete = async (categoryId) => {
+    await deleteCategory(categoryId);
+    getAndSetCategories();
+  };
+
+  return (
+    <section>
+      <h1>Categories</h1>
+      {categories?.map((category) => {
+        return (
+          <div key={category.id}>
+            <ul>
+              <li>
+                <Link to={`/categories/${category.id}`}>{category.label}</Link>{' '}
+                <button onClick={() => handleDelete(category.id)}>
+                  Delete
+                </button>
+              </li>
+            </ul>
+          </div>
+        );
+      })}
+    </section>
+  );
+};
