5ef4a68d2d1dc01cede789256ad27c4624820944
Zach Mosher <zmosher4@gmail.com>
2024-09-04 10:36:01 -0500

    date is displayed with comment

--- src/components/comments/commentList.jsx
+++ src/components/comments/commentList.jsx
@@ -4,6 +4,7 @@ import { useEffect, useRef, useState } from 'react';
 import { Link, useParams } from 'react-router-dom';
 import { addComment, getAllComments } from '../../managers/CommentManager';
 import { getPost } from '../../managers/PostManager';
+import { HumanDate } from '../utils/HumanDate';
 
 export const AllComments = ({ token }) => {
   const [comments, setComments] = useState([]);
@@ -35,6 +36,7 @@ export const AllComments = ({ token }) => {
       author_id: token,
       post_id: postId,
       content: content.current.value,
+      created_on: new Date().toDateString(),
     };
     await addComment(newCommentObject);
     content.current.value = '';
@@ -65,7 +67,18 @@ export const AllComments = ({ token }) => {
             <div key={comment.id}>
               <ul>
                 <li>{comment.content}</li>
-                <li>{comment.username}</li>
+                <li>Commented by: {comment.username}</li>
+                <li>
+                  <li>
+                    {comment.created_on ? (
+                      <>
+                        Comment date: <HumanDate date={comment.created_on} />
+                      </>
+                    ) : (
+                      ''
+                    )}
+                  </li>
+                </li>
               </ul>
             </div>
           );
